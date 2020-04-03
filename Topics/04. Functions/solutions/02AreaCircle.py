e8ebda77c76f8aefd375cca6412f0bee36f15f27
macm_generation@outlook.es <macm_generation@outlook.es>
2020-04-03 11:05:53 +0200

    Functions solutions

--- Topics/04. Functions/solutions/02AreaCircle.py
+++ Topics/04. Functions/solutions/02AreaCircle.py
@@ -3,5 +3,5 @@ import math
 def area_of_circle(r):
     return math.pi * r **2
 
-area = int(input('Enter radius: '))
-print('The area is: ', area_of_circle(area))
+radius = int(input('Enter radius: '))
+print('The area is: ', area_of_circle(radius))
--- Topics/04. Functions/solutions/04Triangular.py
+++ Topics/04. Functions/solutions/04Triangular.py
@@ -1,5 +1,5 @@
 def print_triangular_numbers(n):
-    if n <= 0
+    if n <= 0:
         return
     for i in range(1, n + 1):
         print(i, (i * (i + 1)) // 2)
